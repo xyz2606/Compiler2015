373951d6309d1451d56090c6eedd6c84ff6fa441
高 宇 <pmkfb@aliyun.com>
2015-04-14 23:54:46 +0800

    test1

---
+++ test1.c
@@ -0,0 +1,33 @@
+int g(int x) {
+	return 100;
+}
+struct {
+	int x;
+} f(struct X{int x} b) {
+	int g(int x) {
+		return 100;
+	}
+}
+struct X {
+	struct {
+		int a, b, c
+	}
+} x;
+struct A {
+	int
+};
+int main() {
+	char ff(int x);
+	{
+		char ff(int x) {
+			return 'a';
+		}
+	}
+	ff(1);
+	int x(int x) {
+		return x;
+	}
+	struct {
+		int x;
+	} a;
+}
